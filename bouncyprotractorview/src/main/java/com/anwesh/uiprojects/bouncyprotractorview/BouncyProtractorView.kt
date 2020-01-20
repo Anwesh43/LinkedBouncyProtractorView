6bbf9b8935a174277c327807be4155ccf25a839c
Anwesh43 <anweshthecool0@gmail.com>
2020-01-20 10:37:34 +0530

    adding Int and Float extension methods

--- bouncyprotractorview/src/main/java/com/anwesh/uiprojects/bouncyprotractorview/BouncyProtractorView.kt
+++ bouncyprotractorview/src/main/java/com/anwesh/uiprojects/bouncyprotractorview/BouncyProtractorView.kt
@@ -21,3 +21,8 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 val foreColor : Int = Color.parseColor("#311B92")
 val strokeFactor : Int = 90
 val delay : Long = 20
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
