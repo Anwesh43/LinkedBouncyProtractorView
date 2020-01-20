2f7143dac1b2def1d57684317d6e5c2f3d024c51
Anwesh43 <anweshthecool0@gmail.com>
2020-01-20 10:46:00 +0530

    adding BouncyProtractorView class

--- bouncyprotractorview/src/main/java/com/anwesh/uiprojects/bouncyprotractorview/BouncyProtractorView.kt
+++ bouncyprotractorview/src/main/java/com/anwesh/uiprojects/bouncyprotractorview/BouncyProtractorView.kt
@@ -59,3 +59,21 @@ fun Canvas.drawBPNode(i : Int, scale : Float, paint : Paint) {
     drawBouncyProtractor(scale, size, paint)
     restore()
 }
+
+class BouncyProtractorView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
