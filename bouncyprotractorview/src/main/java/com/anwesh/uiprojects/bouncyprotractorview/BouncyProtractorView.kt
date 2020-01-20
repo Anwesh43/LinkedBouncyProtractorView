1ed1c42e89849a007baa48e81876df9124676230
Anwesh43 <anweshthecool0@gmail.com>
2020-01-20 10:44:10 +0530

    adding Canvas extension methods

--- bouncyprotractorview/src/main/java/com/anwesh/uiprojects/bouncyprotractorview/BouncyProtractorView.kt
+++ bouncyprotractorview/src/main/java/com/anwesh/uiprojects/bouncyprotractorview/BouncyProtractorView.kt
@@ -21,8 +21,41 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 val foreColor : Int = Color.parseColor("#311B92")
 val strokeFactor : Int = 90
 val delay : Long = 20
+val rFactor : Float = 3.2f
+val deg : Float = -60f
+val sweepDeg : Float = 360f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawProtractorLine(j : Int, sf : Float, size : Float, paint : Paint) {
+    save()
+    rotate(deg * (1f - 2 * j) * sf.divideScale(j, lines))
+    drawLine(0f, 0f, 0f, size, paint)
+    restore()
+}
+
+fun Canvas.drawBouncyProtractor(scale : Float, size : Float, paint : Paint) {
+    val r : Float = size / rFactor
+    val sf : Float = scale.sinify()
+    for (j in 0..(lines - 1)) {
+        drawProtractorLine(j, sf, size, paint)
+    }
+    drawArc(RectF(-r, -r, r, r), 0f, sweepDeg * sf, true, paint)
+}
+
+fun Canvas.drawBPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawBouncyProtractor(scale, size, paint)
+    restore()
+}
