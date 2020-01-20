194faf199a1a16d832b3f4f6a4e50a6f0db320b0
Anwesh43 <anweshthecool0@gmail.com>
2020-01-20 10:53:30 +0530

    adding BPNode class

--- bouncyprotractorview/src/main/java/com/anwesh/uiprojects/bouncyprotractorview/BouncyProtractorView.kt
+++ bouncyprotractorview/src/main/java/com/anwesh/uiprojects/bouncyprotractorview/BouncyProtractorView.kt
@@ -125,4 +125,46 @@ class BouncyProtractorView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BPNode(var i : Int, val state : State = State()) {
+
+        private var next : BPNode? = null
+        private var prev : BPNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BPNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBPNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BPNode {
+            var curr : BPNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
